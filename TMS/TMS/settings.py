03b4f8014e8528bc9dee86c6d722666724689064
aneetan <anitaneupane2061@gmail.com>
2025-01-23 21:53:49 +0545

    Load data to website

--- TMS/TMS/settings.py
+++ TMS/TMS/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/authenticate/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- TMS/authenticate/views.py
+++ TMS/authenticate/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import GeneralUser
+from .models import GeneralUser, UserProfileImage
 from .forms import UserProfileForm
 from django.contrib.auth.hashers import make_password, check_password
 
@@ -36,12 +36,11 @@ def process_login(request):
         password = request.POST.get('password')
 
         user = GeneralUser.objects.get(email=email)
-        print(user)
-        print(user.password)
-        print(check_password(password, user.password))
+        profile = UserProfileImage.objects.get(userId=user.id)
+        
         if check_password(password, user.password):
             request.session['id'] = user.id
-            return render(request, 'index.html', {'user': user})
+            return render(request, 'index.html', {'user': user, 'profile': profile})
         
         else:
             return render(request, 'login.html', {'error': 'Invalid email or password'})
