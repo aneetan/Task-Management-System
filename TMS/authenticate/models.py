ab61959b2b98d974df05269523c06ac2e60f4b4b
aneetan <anitaneupane2061@gmail.com>
2025-01-24 22:27:47 +0545

    Models added

--- TMS/authenticate/models.py
+++ TMS/authenticate/models.py
@@ -19,3 +19,29 @@ class UserProfileImage(models.Model):
 
     def __str__(self):
         return f"{self.userId.name}'s Profile"
+
+class Project(models.Model):
+    projectId = models.AutoField(primary_key=True)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_by = models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectUserRole(models.Model):
+    ROLE_CHOICES= [
+        ('Admin', 'Admin'),
+        ('Team Member', 'Team Member')
+    ]
+
+    project_user_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
+    project = models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices= ROLE_CHOICES)
+
+    def __str__(self):
+        return self.role
